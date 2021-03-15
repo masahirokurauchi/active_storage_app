833aa650a4451d666c2b3ee26f7e6e4f422e4942
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-15 15:56:41 +0900

    define methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
-  def index
+  def index 
+    @messages = Message.all
+  end
+
+  def new 
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to root_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
