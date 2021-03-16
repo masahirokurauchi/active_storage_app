59b554ae754003ed30153caf4fa27732e07dd3a9
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-16 15:26:15 +0900

    define edit and update action on message con.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+
   def index 
     @messages = Message.all
   end
@@ -16,8 +18,20 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @message.update(message_params)
+    redirect_to root_path
+  end
+
   private
   def message_params
     params.require(:message).permit(:content, :image)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
